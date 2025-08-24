95c81471da89df8b04d8eecffc202ac46519acef
Sawblaxe <hellvroleplay@gmail.com>
2025-08-24 13:42:27 +0300

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,21 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Sawblaxe'
+description 'Fivem Personal Storage'
+version '1.0.0'
+lua54 'yes'
+
+shared_scripts {
+    'config.lua',
+    'locales/tr.lua',
+    'locales/en.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
+
+server_scripts {
+    'server/main.lua'
+}
